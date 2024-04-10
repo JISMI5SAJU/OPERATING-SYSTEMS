a49a6295412c19804c2ef26ab0d3e1cf275f3335
JISMI5SAJU <146074614+JISMI5SAJU@users.noreply.github.com>
2024-04-10 12:15:52 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,34 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-int n;
-printf("Enter the no of processes");
-scanf("%d",&n);
-int p[n];
-int bt[n],wt[n],tat[n];
-int tot_wt=0,tot_tat;
-printf("Enter the process names and burst time of each processses\n");
-for(int i=0;i<n;i++)
-{
-scanf("%d",&p[i]);
-scanf("%d",&bt[i]);
+    int n,i;
+    printf("Enter no of processes");
+    scanf("%d",&n);
+    int p[n],bt[n],wt[n],tat[n];
+    printf("Enter the process and their corresponding burst times\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&p[i]);
+        scanf("%d",&bt[i]);
+    }
+    wt[0]=0;
+    tat[0]=bt[0];
+    
+    float totwt=wt[0],tottat=bt[0];
+    
+    for(int i=1;i<n;i++)
+    {
+        wt[i]=wt[i-1]+bt[i-1];
+        tat[i]=tat[i-1]+bt[i];
+        totwt=totwt+wt[i];
+        tottat=tottat+tat[i];
+    }
+    printf("process\tbt\twt\ttat\n");
+    for(i=0;i<n;i++)
+    {
+    printf(" %d\t   %d\t  %d\t %d\n",p[i],bt[i],wt[i],tat[i]);
+    }
+    printf("Average waiting time=%f\nAverage turnaroundTime=%f\n",totwt/n,tottat/n);
 }
-wt[0]=0;
-tat[0]=bt[0];
-tot_tat=tat[0];
-printf("Process name:%d\nProcess Burst Time:%d\nProcess Waiting Time:%d\nProcess TuranAroundTime:%d\n",p[0],bt[0],wt[0],tat[0]);
-for(int i=1;i<n;i++)
-{
-wt[i]=wt[i-1]+bt[i-1];
-tot_wt=tot_wt+wt[i];
-tat[i]=tat[i-1]+bt[i];
-tot_tat=tot_tat+tat[i];
-printf("Process name:%d\nProcess Burst Time:%d\nProcess Waiting Time:%d\nProcess TuranAroundTime:%d\n",p[i],bt[i],wt[i],tat[i]);
-}
-printf("Total Waiting Time:%d\nTotal TurnAround Time:%d\n",tot_wt,tot_tat);
-float av_wt,av_tat;
-av_wt=(float)tot_wt/n;
-av_tat=(float)tot_tat/n;
-printf("Average Waitong Time:%f\nAverage TurnAround Time:%f\n",av_wt,av_tat);
-}
+
+output:
+Enter no of processes3
+Enter the process and their corresponding burst times
+1
+5
+2
+10
+3
+3
+process	bt	wt	tat
+ 1	   5	  0	 5
+ 2	   10	  5	 15
+ 3	   3	  15	 18
+Average waiting time=6.666667
+Average turnaroundTime=12.666667
 
