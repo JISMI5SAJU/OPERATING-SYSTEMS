9593f277079656eb6367f8b71e21e3845eae3945
JISMI5SAJU <146074614+JISMI5SAJU@users.noreply.github.com>
2024-07-06 16:15:59 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,51 +1,63 @@
-#include <stdio.h>
-
-void main() {
-    printf("Enter number of process\n");
-    int limit;
-    scanf("%d", &limit);
+#include<stdio.h>
+void main()
+{
+    int n,i,j,temp;
+    printf("Enter the number of processses\n");
+    scanf("%d",&n);
+    int p[n],bt[n],wt[n],tat[n],priority[n];
+    printf("Enter process,burst time and Priority\n");
+    for(i=0;i<n;i++)
+    scanf("%d%d%d",&p[i],&bt[i],&priority[i]);
     
-    int process[limit], bt[limit], wt[limit], tat[limit], priority[limit], i, j, temp;
-    
-    printf("Enter processes, burst time, and priority\n");
-    for (i = 0; i < limit; i++) {
-        scanf("%d%d%d", &process[i], &bt[i], &priority[i]);
-    }
-    
-    for (i = 0; i < limit - 1; i++) {
-        for (j = 0; j < limit - i - 1; j++) {
-            if (priority[j] > priority[j + 1]) {
-                temp = priority[j];
-                priority[j] = priority[j + 1];
-                priority[j + 1] = temp;
+    for(i=0;i<n;i++){
+        for(j=0;j<n-i-1;j++){
+            if(priority[j]>priority[j+1]){
                 
-                temp = process[j];
-                process[j] = process[j + 1];
-                process[j + 1] = temp;
+                temp=priority[j];
+                priority[j]=priority[j+1];
+                priority[j+1]=temp;
                 
-                temp = bt[j];
-                bt[j] = bt[j + 1];
-                bt[j + 1] = temp;
+                temp=bt[j];
+                bt[j]= bt[j+1];
+                bt[j+1]=temp;
+                
+                temp=p[j];
+                p[j]=p[j+1];
+                p[j+1]=temp;
             }
         }
     }
-    
-    wt[0] = 0; 
-    tat[0] = bt[0]; 
-
-    float tot_wt = wt[0], tot_tat = tat[0]; 
-
-    for (i = 1; i < limit; i++) { 
-        wt[i] = wt[i - 1] + bt[i - 1];
-        tat[i] = tat[i - 1] + bt[i];
-        tot_wt += wt[i];
-        tot_tat += tat[i];
-    }
-
-    printf("Process\tBT\tPriority\tWT\tTAT\n");
-    for (i = 0; i < limit; i++) {
-        printf("%d\t%d\t%d\t\t%d\t%d\n", process[i], bt[i], priority[i], wt[i], tat[i]);
-    }
-
-    printf("Average WT = %.2f\nAverage TAT = %.2f\n", tot_wt / limit, tot_tat / limit);
+  wt[0]=0,tat[0]=bt[0];
+  
+  float tot_wt=wt[0],tot_tat=tat[0];
+  
+  for(i=1;i<n;i++){
+      wt[i]=wt[i-1]+bt[i-1];
+      tat[i]=tat[i-1]+bt[i];
+      tot_wt=tot_wt+wt[i];
+      tot_tat=tot_tat+tat[i];
+  }
+  printf("Process\tbt\tPriority\twt\ttat\n");
+  for(int i=0;i<n;i++){
+      printf("%d\t%d\t%d\t\t%d\t%d\n",p[i],bt[i],priority[i],wt[i],tat[i]);
+  }
+  printf("Average waiting time=%f\n",tot_wt/n);
+   printf("Average TurnAround time=%f\n",tot_tat/n);
 }
+
+OUTPUT:
+Enter the number of processses
+3
+Enter process,burst time and Priority
+1 10 2
+2 5 1
+3 3 3
+Process bt      Priority        wt      tat
+2       5       1               0       5
+1       10      2               5       15
+3       3       3               15      18
+Average waiting time=6.666667
+Average TurnAround time=12.666667
+               
+
+   
